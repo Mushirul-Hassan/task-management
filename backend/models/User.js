773af23a3a0fae6d1cf972347b05284bd50a011a
Mushirul Hassan <mushirulhassan2@gmail.com>
2026-06-07 21:31:44 +0530

    Added presave hook for password hashing and also password comparison

--- backend/models/User.js
+++ backend/models/User.js
@@ -27,3 +27,16 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true },
 );
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password);
+};
+
+module.exports = mongoose.model("User", userSchema);
