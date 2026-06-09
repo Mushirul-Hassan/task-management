f4ed36b9e37c1ec7a6f6b71980d8ad3acb8e3c8a
Mushirul Hassan <mushirulhassan2@gmail.com>
2026-06-09 17:40:08 +0530

    made api service file for tasks

---
+++ task-manager-frontend/src/api/task.api.js
@@ -0,0 +1,9 @@
+import api from "./axios";
+
+export const getTasks = (params) => api.get("/tasks", { params });
+export const getTaskById = (id) => api.get(`/tasks/${id}`);
+export const createTask = (data) => api.post("/tasks", data);
+export const updateTask = (id, data) => api.put(`/tasks/${id}`, data);
+export const deleteTask = (id) => api.delete(`/tasks/${id}`);
+export const toggleTask = (id) => api.patch(`/tasks/${id}/toggle`);
+export const getStats = () => api.get("/tasks/stats");
